b3e80e8690f9c306d8d2877eeb600992ba9f70a5
Mark Azuolas <markazuolas@gmail.com>
2017-09-28 19:34:15 -0700

    fix typos

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,10 +6,10 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_lenght=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_lenght = 200)
+    choice_text = models.CharField(max_length = 200)
     tally = models.IntgerField(default=0)
