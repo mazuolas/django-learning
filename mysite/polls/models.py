ebc2e187d1045af0f8d756b2510d3472e351b59c
Mark Azuolas <markazuolas@gmail.com>
2017-09-28 19:33:41 -0700

    fix models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 # Create your models here.
 
-class Question(models.Models):
+class Question(models.Model):
     question_text = models.CharField(max_lenght=200)
     pub_date = models.DateTimeField('date published')
 
-class Choice(models.Models):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_lenght = 200)
     tally = models.IntgerField(default=0)
