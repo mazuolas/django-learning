a6df6fc80f297da2a4ffeb6a0a03664d38d4250c
Mark Azuolas <markazuolas@gmail.com>
2017-09-28 18:45:10 -0700

    add questions model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Models):
+    question_text = models.CharField(max_lenght=200)
+    pub_date = models.DateTimeField('date published')
