e4b971920843827ebcdf3b9eec4dde30546c203b
Mark Azuolas <markazuolas@gmail.com>
2017-09-28 20:14:52 -0700

    add detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,3 +9,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
