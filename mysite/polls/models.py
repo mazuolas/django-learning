69315abab3a386a8999d00a5ab4418cd94e0e87f
Mark Azuolas <markazuolas@gmail.com>
2017-09-28 18:49:07 -0700

    add choice model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,3 +8,8 @@ from django.db import models
 class Question(models.Models):
     question_text = models.CharField(max_lenght=200)
     pub_date = models.DateTimeField('date published')
+
+class Choice(models.Models):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_lenght = 200)
+    tally = models.IntgerField(default=0)
